6be0bb0b3ff923422a62d06ece6a320232593b4e
javante_nelson <javantenelson@gmail.com>
2020-05-07 00:40:46 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+
+    def self.current_user(hash)
+      #binding.pry
+      @user = User.find(hash[:user_id])  
+    end 
+
+    def self.is_logged_in?(hash)
+        !!hash[:user_id]
+
+    end 
 end
